8ab3bd9a282beabc6b5d2cde06336b53cb577946
songyouwei <youwei0314@gmail.com>
2018-05-24 23:16:04 +0800

    add projection for multi-head attention & add SelfAttention

--- layers/attention.py
+++ layers/attention.py
@@ -10,17 +10,24 @@ import torch.nn.functional as F
 
 
 class Attention(nn.Module):
-    def __init__(self, embed_dim, hidden_dim=None, n_head=1, score_function='scaled_dot_product'):
-        # score_function: scaled_dot_product / mlp (concat) / bi_linear (general dot)
+    def __init__(self, embed_dim, hidden_dim=None, n_head=1, score_function='scaled_dot_product', dropout=0.1):
+        ''' Attention Mechanism
+        :param embed_dim:
+        :param hidden_dim:
+        :param n_head: num of head (Multi-Head Attention)
+        :param score_function: scaled_dot_product / mlp (concat) / bi_linear (general dot)
+        '''
         super(Attention, self).__init__()
         if hidden_dim is None:
-            hidden_dim = embed_dim
+            hidden_dim = embed_dim // n_head
         self.embed_dim = embed_dim
         self.hidden_dim = hidden_dim
         self.n_head = n_head
         self.score_function = score_function
         self.w_kx = nn.Parameter(torch.FloatTensor(n_head, embed_dim, hidden_dim))
         self.w_qx = nn.Parameter(torch.FloatTensor(n_head, embed_dim, hidden_dim))
+        self.proj = nn.Linear(n_head * hidden_dim, embed_dim)
+        self.dropout = nn.Dropout(dropout)
         if score_function == 'mlp':
             self.weight = nn.Parameter(torch.Tensor(hidden_dim*2, 1))
         elif self.score_function == 'bi_linear':
@@ -41,7 +48,7 @@ class Attention(nn.Module):
         # kx: (n_head, ?*k_len, embed_dim) -> (n_head*?, k_len, hidden_dim)
         # qx: (n_head, ?*q_len, embed_dim) -> (n_head*?, q_len, hidden_dim)
         # score: (n_head*?, q_len, k_len,)
-        # output: (?, q_len, n_head*hidden_dim,)
+        # output: (?, q_len, embed_dim,)
         kx = k.repeat(self.n_head, 1, 1).view(self.n_head, -1, self.embed_dim)  # (n_head, ?*k_len, embed_dim)
         qx = q.repeat(self.n_head, 1, 1).view(self.n_head, -1, self.embed_dim)  # (n_head, ?*q_len, embed_dim)
         kx = torch.bmm(kx, self.w_kx).view(-1, k_len, self.hidden_dim)  # (n_head*?, k_len, hidden_dim)
@@ -56,12 +63,27 @@ class Attention(nn.Module):
             kq = torch.cat((kxx, qxx), dim=-1)  # (n_head*?, q_len, k_len, hidden_dim*2)
             score = F.tanh(torch.matmul(kq, self.weight).squeeze(dim=-1))
         elif self.score_function == 'bi_linear':
-            qw = torch.matmul(q, self.weight)
-            kt = k.permute(0, 2, 1)
+            qw = torch.matmul(qx, self.weight)
+            kt = kx.permute(0, 2, 1)
             score = torch.bmm(qw, kt)
         else:
             raise RuntimeError('invalid score_function')
         score = F.softmax(score, dim=-1)
         output = torch.bmm(score, kx)  # (n_head*?, q_len, hidden_dim)
-        output = torch.cat(torch.split(output, mb_size, dim=0), dim=-1)
+        output = torch.cat(torch.split(output, mb_size, dim=0), dim=-1)  # (?, q_len, n_head*hidden_dim)
+        output = self.proj(output)  # (?, q_len, embed_dim)
+        output = self.dropout(output)
         return output
+
+
+class SelfAttention(Attention):
+    '''q is a parameter'''
+    def __init__(self, embed_dim, hidden_dim=None, n_head=1, score_function='scaled_dot_product', q_len=1, dropout=0.1):
+        super(SelfAttention, self).__init__(embed_dim, hidden_dim, n_head, score_function, dropout)
+        self.q_len = q_len
+        self.q = nn.Parameter(torch.FloatTensor(q_len, embed_dim))
+
+    def forward(self, k, **kwargs):
+        mb_size = k.shape[0]
+        q = self.q.expand(mb_size, -1, -1)
+        return super(SelfAttention, self).forward(k, q)

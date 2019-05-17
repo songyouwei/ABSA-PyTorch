1139cffab26a54c5ec1cf96e2e54d86d7e3c6247
songyouwei <youwei0314@gmail.com>
2019-05-17 18:09:50 +0800

    Add validation support
    * validation support (random split from testset)
    
    * remove tensorboardX, use logger instead
    
    * add random seed support

--- train.py
+++ train.py
@@ -2,21 +2,30 @@
 # file: train.py
 # author: songyouwei <youwei0314@gmail.com>
 # Copyright (C) 2018. All Rights Reserved.
+import logging
+import argparse
+import math
+import os
+import sys
+from time import strftime, localtime
+import random
+import numpy
+
 from pytorch_pretrained_bert import BertModel
 from sklearn import metrics
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
-from tensorboardX import SummaryWriter
-import argparse
-import math
-import os
+from torch.utils.data import DataLoader, random_split
 
 from data_utils import build_tokenizer, build_embedding_matrix, Tokenizer4Bert, ABSADataset
 
 from models import LSTM, IAN, MemNet, RAM, TD_LSTM, Cabasc, ATAE_LSTM, TNet_LF, AOA, MGAN
 from models.aen import CrossEntropyLoss_LSR, AEN, AEN_BERT
 from models.bert_spc import BERT_SPC
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 class Instructor:
@@ -26,9 +35,6 @@ class Instructor:
         if 'bert' in opt.model_name:
             tokenizer = Tokenizer4Bert(opt.max_seq_len, opt.pretrained_bert_name)
             bert = BertModel.from_pretrained(opt.pretrained_bert_name)
-            # freeze pretrained bert params
-            # for param in bert.parameters():
-            #     param.requires_grad = False
             self.model = opt.model_class(bert, opt).to(opt.device)
         else:
             tokenizer = build_tokenizer(
@@ -43,11 +49,18 @@ class Instructor:
 
         trainset = ABSADataset(opt.dataset_file['train'], tokenizer)
         testset = ABSADataset(opt.dataset_file['test'], tokenizer)
+        assert 0 <= opt.valset_ratio < 1
+        if opt.valset_ratio > 0:
+            valset_len = int(len(trainset) * opt.valset_ratio)
+            trainset, valset = random_split(trainset, (len(trainset)-valset_len, valset_len))
+        else:
+            valset = testset
         self.train_data_loader = DataLoader(dataset=trainset, batch_size=opt.batch_size, shuffle=True)
         self.test_data_loader = DataLoader(dataset=testset, batch_size=opt.batch_size, shuffle=False)
+        self.val_data_loader = DataLoader(dataset=valset, batch_size=opt.batch_size, shuffle=False)
 
         if opt.device.type == 'cuda':
-            print("cuda memory allocated:", torch.cuda.memory_allocated(device=opt.device.index))
+            logger.info('cuda memory allocated: {}'.format(torch.cuda.memory_allocated(device=opt.device.index)))
         self._print_args()
 
     def _print_args(self):
@@ -58,14 +71,14 @@ class Instructor:
                 n_trainable_params += n_params
             else:
                 n_nontrainable_params += n_params
-        print('n_trainable_params: {0}, n_nontrainable_params: {1}'.format(n_trainable_params, n_nontrainable_params))
-        print('> training arguments:')
+        logger.info('n_trainable_params: {0}, n_nontrainable_params: {1}'.format(n_trainable_params, n_nontrainable_params))
+        logger.info('> training arguments:')
         for arg in vars(self.opt):
-            print('>>> {0}: {1}'.format(arg, getattr(self.opt, arg)))
+            logger.info('>>> {0}: {1}'.format(arg, getattr(self.opt, arg)))
 
     def _reset_params(self):
         for child in self.model.children():
-            if type(child) != BertModel:  # skip bert params (with unfreezed bert)
+            if type(child) != BertModel:  # skip bert params
                 for p in child.parameters():
                     if p.requires_grad:
                         if len(p.shape) > 1:
@@ -75,19 +88,19 @@ class Instructor:
                             torch.nn.init.uniform_(p, a=-stdv, b=stdv)
 
     def _train(self, criterion, optimizer):
-        writer = SummaryWriter(log_dir=self.opt.logdir)
-        max_test_acc = 0
-        max_f1 = 0
+        max_val_acc = 0
+        max_val_f1 = 0
         global_step = 0
+        path = None
         for epoch in range(self.opt.num_epoch):
-            print('>' * 100)
-            print('epoch: ', epoch)
+            logger.info('>' * 100)
+            logger.info('epoch: {}'.format(epoch))
             n_correct, n_total = 0, 0
+            # switch model to training mode
+            self.model.train()
             for i_batch, sample_batched in enumerate(self.train_data_loader):
                 global_step += 1
-
-                # switch model to training mode, clear gradient accumulators
-                self.model.train()
+                # clear gradient accumulators
                 optimizer.zero_grad()
 
                 inputs = [sample_batched[col].to(self.opt.device) for col in self.opt.inputs_cols]
@@ -103,38 +116,35 @@ class Instructor:
                     n_total += len(outputs)
                     train_acc = n_correct / n_total
 
-                    # switch model to evaluation mode
-                    self.model.eval()
-                    test_acc, f1 = self._evaluate_acc_f1()
-                    if test_acc > max_test_acc:
-                        max_test_acc = test_acc
-                        if not os.path.exists('state_dict'):
-                            os.mkdir('state_dict')
-                        path = 'state_dict/{0}_{1}_acc{2}'.format(self.opt.model_name, self.opt.dataset, round(test_acc, 4))
-                        torch.save(self.model.state_dict(), path)
-                        print('>> saved: ' + path)
-                    if f1 > max_f1:
-                        max_f1 = f1
+                    logger.info('loss: {:.4f}, acc: {:.4f}'.format(loss.item(), train_acc))
 
-                    writer.add_scalar('loss', loss, global_step)
-                    writer.add_scalar('acc', train_acc, global_step)
-                    writer.add_scalar('test_acc', test_acc, global_step)
-                    print('loss: {:.4f}, acc: {:.4f}, test_acc: {:.4f}, f1: {:.4f}'.format(loss.item(), train_acc, test_acc, f1))
+            # switch model to evaluation mode
+            self.model.eval()
+            val_acc, val_f1 = self._evaluate_acc_f1(self.val_data_loader)
+            logger.info('> val_acc: {:.4f}, val_f1: {:.4f}'.format(val_acc, val_f1))
+            if val_acc > max_val_acc:
+                max_val_acc = val_acc
+                if not os.path.exists('state_dict'):
+                    os.mkdir('state_dict')
+                path = 'state_dict/{0}_{1}_val_acc{2}'.format(self.opt.model_name, self.opt.dataset, round(val_acc, 4))
+                torch.save(self.model.state_dict(), path)
+                logger.info('>> saved: {}'.format(path))
+            if val_f1 > max_val_f1:
+                max_val_f1 = val_f1
 
-        writer.close()
-        return max_test_acc, max_f1
+        return path
 
-    def _evaluate_acc_f1(self):
-        n_test_correct, n_test_total = 0, 0
+    def _evaluate_acc_f1(self, data_loader):
+        n_correct, n_total = 0, 0
         t_targets_all, t_outputs_all = None, None
         with torch.no_grad():
-            for t_batch, t_sample_batched in enumerate(self.test_data_loader):
-                t_inputs = [t_sample_batched[col].to(opt.device) for col in self.opt.inputs_cols]
-                t_targets = t_sample_batched['polarity'].to(opt.device)
+            for t_batch, t_sample_batched in enumerate(data_loader):
+                t_inputs = [t_sample_batched[col].to(self.opt.device) for col in self.opt.inputs_cols]
+                t_targets = t_sample_batched['polarity'].to(self.opt.device)
                 t_outputs = self.model(t_inputs)
 
-                n_test_correct += (torch.argmax(t_outputs, -1) == t_targets).sum().item()
-                n_test_total += len(t_outputs)
+                n_correct += (torch.argmax(t_outputs, -1) == t_targets).sum().item()
+                n_total += len(t_outputs)
 
                 if t_targets_all is None:
                     t_targets_all = t_targets
@@ -143,9 +153,9 @@ class Instructor:
                     t_targets_all = torch.cat((t_targets_all, t_targets), dim=0)
                     t_outputs_all = torch.cat((t_outputs_all, t_outputs), dim=0)
 
-        test_acc = n_test_correct / n_test_total
+        acc = n_correct / n_total
         f1 = metrics.f1_score(t_targets_all.cpu(), torch.argmax(t_outputs_all, -1).cpu(), labels=[0, 1, 2], average='macro')
-        return test_acc, f1
+        return acc, f1
 
     def run(self):
         # Loss and Optimizer
@@ -154,24 +164,26 @@ class Instructor:
         optimizer = self.opt.optimizer(_params, lr=self.opt.learning_rate, weight_decay=self.opt.l2reg)
 
         self._reset_params()
-        max_test_acc, max_f1 = self._train(criterion, optimizer)
-        print('max_test_acc: {0}     max_f1: {1}'.format(max_test_acc, max_f1))
+        best_model_path = self._train(criterion, optimizer)
+        self.model.load_state_dict(torch.load(best_model_path))
+        self.model.eval()
+        test_acc, test_f1 = self._evaluate_acc_f1(self.test_data_loader)
+        logger.info('>> test_acc: {:.4f}, test_f1: {:.4f}'.format(test_acc, test_f1))
 
 
-if __name__ == '__main__':
+def main():
     # Hyper Parameters
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', default='lstm', type=str)
+    parser.add_argument('--model_name', default='bert_spc', type=str)
     parser.add_argument('--dataset', default='twitter', type=str, help='twitter, restaurant, laptop')
     parser.add_argument('--optimizer', default='adam', type=str)
     parser.add_argument('--initializer', default='xavier_uniform_', type=str)
-    parser.add_argument('--learning_rate', default=2e-5, type=float)  # try 5e-5, 3e-5, 2e-5 for BERT models (sensitive)
+    parser.add_argument('--learning_rate', default=2e-5, type=float, help='try 5e-5, 2e-5 for BERT, 1e-3 for others')
     parser.add_argument('--dropout', default=0.1, type=float)
     parser.add_argument('--l2reg', default=0.01, type=float)
-    parser.add_argument('--num_epoch', default=20, type=int)
-    parser.add_argument('--batch_size', default=64, type=int)  # try 16, 32, 64 for BERT models
+    parser.add_argument('--num_epoch', default=10, type=int, help='try larger number for non-BERT models')
+    parser.add_argument('--batch_size', default=64, type=int, help='try 16, 32, 64 for BERT models')
     parser.add_argument('--log_step', default=5, type=int)
-    parser.add_argument('--logdir', default='log', type=str)
     parser.add_argument('--embed_dim', default=300, type=int)
     parser.add_argument('--hidden_dim', default=300, type=int)
     parser.add_argument('--bert_dim', default=768, type=int)
@@ -179,8 +191,18 @@ if __name__ == '__main__':
     parser.add_argument('--max_seq_len', default=80, type=int)
     parser.add_argument('--polarities_dim', default=3, type=int)
     parser.add_argument('--hops', default=3, type=int)
-    parser.add_argument('--device', default=None, type=str)
+    parser.add_argument('--device', default=None, type=str, help='e.g. cuda:0')
+    parser.add_argument('--seed', default=None, type=int, help='set seed for reproducibility')
+    parser.add_argument('--valset_ratio', default=0, type=float, help='set ratio between 0 and 1 for validation support')
     opt = parser.parse_args()
+
+    if opt.seed is not None:
+        random.seed(opt.seed)
+        numpy.random.seed(opt.seed)
+        torch.manual_seed(opt.seed)
+        torch.cuda.manual_seed(opt.seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
     model_classes = {
         'lstm': LSTM,
@@ -224,7 +246,7 @@ if __name__ == '__main__':
         'mgan': ['text_raw_indices', 'aspect_indices', 'text_left_indices'],
         'bert_spc': ['text_bert_indices', 'bert_segments_ids'],
         'aen': ['text_raw_indices', 'aspect_indices'],
-        'aen_bert' : ['text_raw_bert_indices', 'aspect_bert_indices'],
+        'aen_bert': ['text_raw_bert_indices', 'aspect_bert_indices'],
     }
     initializers = {
         'xavier_uniform_': torch.nn.init.xavier_uniform_,
@@ -248,5 +270,12 @@ if __name__ == '__main__':
     opt.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') \
         if opt.device is None else torch.device(opt.device)
 
+    log_file = '{}-{}-{}.log'.format(opt.model_name, opt.dataset, strftime("%y%m%d-%H%M", localtime()))
+    logger.addHandler(logging.FileHandler(log_file))
+
     ins = Instructor(opt)
     ins.run()
+
+
+if __name__ == '__main__':
+    main()
